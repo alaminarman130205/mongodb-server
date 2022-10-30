03d036fa378dd8e4c1d4311c0806db708d7108e0
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-30 21:00:16 +0600

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//user : dbuser2
+// password : HsMDmV1Ta8wPC2uP
+
+const uri =
+  "mongodb+srv://dbuser2:HsMDmV1Ta8wPC2uP@cluster0.vycotd7.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("nodeMongoCrud").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("hello from node mongo crud server");

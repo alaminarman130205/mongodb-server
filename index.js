e4cc4311d560bf029f467d2c6811f91ca9fd5587
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-30 22:05:48 +0600

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,14 @@ async function run() {
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      // console.log("trying to delete", id);
+      const result = await userCollection.deleteOne(query);
+      console.log(result);
       res.send(result);
     });
   } finally {
